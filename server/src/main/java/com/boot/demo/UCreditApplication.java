9730d957e35188744aed0bf738134c3c61b38c6d
yangtianfeng <yangtianfeng@youxin.com>
2021-01-20 19:31:08 +0800

    “集成shardingsphere”

--- server/src/main/java/com/boot/demo/UCreditApplication.java
+++ server/src/main/java/com/boot/demo/UCreditApplication.java
@@ -1,5 +1,7 @@
 package com.boot.demo;
 
+import org.apache.shardingsphere.transaction.annotation.ShardingTransactionType;
+import org.apache.shardingsphere.transaction.core.TransactionType;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
