832a89e1f383ebb80104918347722bc65d0656cc
yangtianfeng <yangtianfeng@youxin.com>
2021-02-18 15:16:30 +0800

    “ShardingJDBC 分库分表”

--- server/src/main/java/com/boot/demo/UCreditApplication.java
+++ server/src/main/java/com/boot/demo/UCreditApplication.java
@@ -1,17 +1,18 @@
 package com.boot.demo;
 
-import org.apache.shardingsphere.transaction.annotation.ShardingTransactionType;
-import org.apache.shardingsphere.transaction.core.TransactionType;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication()
 @MapperScan("com.boot.demo.mapper")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class UCreditApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
