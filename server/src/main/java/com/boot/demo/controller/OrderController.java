bd7ccfc1e6a51774cdc1df87df44f5f98cf653af
yangtianfeng <yangtianfeng@youxin.com>
2021-02-19 17:33:03 +0800

    “seata”

---
+++ server/src/main/java/com/boot/demo/controller/OrderController.java
@@ -0,0 +1,27 @@
+package com.boot.demo.controller;
+
+import com.boot.demo.bean.User;
+import com.boot.demo.service.UserService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping(value = "order")
+public class OrderController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("update")
+    String update(@RequestParam("userId") Long userId, @RequestParam("money") BigDecimal money, @RequestParam("status") Integer status){
+        User user = new User();
+        user.setAge(10);
+        userService.insertForeach(user);
+        return "通知成功";
+    }
+}
